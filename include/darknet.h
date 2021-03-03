97fb525a47a3219e5875a0508ad7c0f55b3090a9
cheolsun lim <skycs97@gmail.com>
2021-03-03 13:24:40 +0900

    dd

--- include/darknet.h
+++ include/darknet.h
@@ -27,7 +27,7 @@ extern "C"
     extern int gpu_index;
     extern twin_thpool *twin_thp;
 #define THREAD_NUM_POOL 8
-#define n_loop 16 //hojin # of loop in classifier2
+
 #ifdef THREAD
     extern pthread_cond_t *cond_t;
     extern pthread_mutex_t *mutex_t;
